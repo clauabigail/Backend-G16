3f0eec50e536bfe0edda690b87f622a9bf6a297f
clauabigail <clauabigail@gmail.com>
2024-01-11 21:06:27 -0500

    inicios en python

---
+++ Dia 1/2_colecciones_de_datos.py
@@ -0,0 +1,20 @@
+# Puedo agrupar varios valores en una variable
+
+# Listas
+# Que se puede modificar, es ordenada (maneja indices)
+alumnos = ['Victor', 'Hiroito' , 'Marco' , 'Angel' , 'Bryan' , 'Samael' ,'Claudia' ]
+
+# Las listas empiezan con la posicion 0
+print (alumnos[0])
+print (alumnos[4])
+
+# Para saber el contenido (longitud) de datos
+# cuenta y no utiliza las posiciones
+print(len(alumnos))
+
+# Si queremos recorrer la lista de derecha a izq itilizaremos numeros negativos
+print(alumnos[-1])
+
+print(alumnos[len(alumnos)-1]) #otra forma, menos eficiente de obtener el ultimo elemento
+
+
