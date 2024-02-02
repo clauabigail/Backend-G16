904c9c66f5c396dbd6e26592509e8d03632bd42a
clauabigail <clauabigail@gmail.com>
2024-02-02 17:50:19 -0500

    añadi carpeta mirations  de flask db init

---
+++ models/trago.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, types
+from variables import conexion
+
+
+class Trago(conexion.Model):
+    # para indicar como se llamara esta tabla en la bd
+    __tablename__ = 'tragos'
+
+    id = Column(type_=types.Integer, autoincrement=True, primary_key=True)
+
+    nombre = Column(type_=types.Text, nullable=False)
+    # server_default > sirve para indicar el valor por defecto A NIVEL DE BD en el caso que no se ingrese al momento de hacer una creacion
+    # server_default solo acepta string o TextClause
+    disponible = Column(type_=types.Boolean, server_default='true')
