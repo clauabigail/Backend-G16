2167b82529c393bfc09e05fb83697e17e994e0fd
clauabigail <clauabigail@gmail.com>
2024-02-07 21:37:46 -0500

    semana05

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from os import environ
 from models import *
 from controllers import *
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 # leear el archivo .env si existe y agregara todas las variables al entorno como si fuesen variables de entorno del sistema
 # tiene que ir en la parte mas alta del archivo principal para que pueda ser utilizado en todo el proyecto
@@ -17,13 +19,23 @@ api = Api(app=app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 
+# configuraciones para mi JWT
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1, minutes=30)  # 90
+
 conexion.init_app(app)
 
+
+JWTManager(app=app)
 # Esto crea la utilizacion de las migraciones en nuestro proyecto de flask
 Migrate(app=app, db=conexion)
 
 # Agrego mis rutas
 api.add_resource(InvitadosController, '/invitados')
+api.add_resource(BarmanController, '/barman')
+api.add_resource(LoginController, '/login')
+api.add_resource(LoginInvitadoController, '/login-invitado')
+api.add_resource(PedidosController, '/pedidos')
 
 if __name__ == "__main__":
     app.run(debug=True)
