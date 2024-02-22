7663a898d57be71b303a56f43d9c365a8a2e8bec
clauabigail <clauabigail@gmail.com>
2024-02-21 19:13:24 -0500

    semana 06 terminada

---
+++ gestion/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+def vistaPrueba(request):
+    usuario = {
+        'nombre': 'Claudia',
+        'apellido': 'Abril',
+        'hobbies': [
+            {
+                'descripcion': 'Ir al cine'
+            },
+            {
+                'descripcion': 'Viajar'
+            }
+            
+            ]
+        
+    }
+    return render(request=request, template_name='prueba.html', context=usuario)
+
+def mostrarRecetas(request):
+    return render(request, 'mostrarRecetas.html')
+
+# siempre en una funcion que trabaja como controlador recibiremos el request (informacion entrante del cliente)
+@api_view(http_method_names=['GET', 'POST'])
+def controlladorInicial(request):
+    return Response(data={
+        'message' : 'Bienvenido a mi API'
+    })
